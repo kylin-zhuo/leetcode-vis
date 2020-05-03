f1d0639d4e01e3e23678955e14450ea2d2102a31
kylin-zhuo <lin.zhuo@hotmail.com>
2020-05-02 21:41:26 -0700

    Update.

--- app.py
+++ app.py
@@ -10,9 +10,14 @@ def home():
     return render_template('home.html')
 
 @app.route('/1.html')
-def test1():
+def problem1():
 	# form = InputStringForm()
 	return render_template('1.html')
+
+@app.route('/2.html')
+def problem2():
+	# form = InputStringForm()
+	return render_template('2.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
--- static/js/1.js
+++ static/js/1.js
@@ -1,29 +1,103 @@
 
+
 var input = [8,1,4,9,7,0];
 
-var radius = 20;
+// var radius = 20;
 
-console.log(input);
+// console.log(input);
 
-var jsonCircles = [
-    { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "white" },
-    { "x_axis": 80, "y_axis": 30, "radius": 20, "color" : "white"},
-    { "x_axis": 130, "y_axis": 30, "radius": 20, "color" : "white"},
-    { "x_axis": 180, "y_axis": 30, "radius": 20, "color" : "white"},
-    { "x_axis": 230, "y_axis": 30, "radius": 20, "color" : "white"},
-    { "x_axis": 280, "y_axis": 30, "radius": 20, "color" : "white"},
-    { "x_axis": 330, "y_axis": 30, "radius": 20, "color" : "white"}
-    ];
 
-var svgContainer = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
+var preprocess = function(input) {
+    var res = [];
+    for (var i = 0; i < input.length; i++) {
+        var obj = {"val" : input[i]};
+        res.push(obj);
+    }
+    return res;
+}
 
-var circles = svgContainer.selectAll("circle")
-                    .data(jsonCircles)
-                    .enter()
-                    .append("circle");
+var jsonInput = preprocess(input); 
+console.log(jsonInput);
 
-var circleAttributes = circles
-                .attr("cx", function (d) { return d.x_axis; })
-                .attr("cy", function (d) { return d.y_axis; })
-                .attr("r", function (d) { return d.radius; })
-                .style("fill", function(d) { return d.color; });
+// var jsonCircles = [
+//     { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "white" },
+//     { "x_axis": 80, "y_axis": 30, "radius": 20, "color" : "white"},
+//     { "x_axis": 130, "y_axis": 30, "radius": 20, "color" : "white"},
+//     { "x_axis": 180, "y_axis": 30, "radius": 20, "color" : "white"},
+//     { "x_axis": 230, "y_axis": 30, "radius": 20, "color" : "white"},
+//     { "x_axis": 280, "y_axis": 30, "radius": 20, "color" : "white"},
+//     { "x_axis": 330, "y_axis": 30, "radius": 20, "color" : "white"}
+//     ];
+
+// var svg = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
+
+var distance = 50;
+
+function updateChart() {
+
+    console.log("aaa");
+
+    var svg = d3.select("#d3chart");
+    var groups = svg.selectAll(".groups")
+                        .data(jsonInput, function(d) {
+                            return d.val;
+                        })
+                        .enter()
+                        .append("g")
+                        .attr("class", "gbar");
+
+    // var enterG = binding.enter().append("g");
+
+    enterG.append("rect").attr("width", 40).attr("height", 40);
+
+    enterG.append("text")
+     .attr("x", 3)
+     .attr("y", 3)
+     .text(function(d) {return d.val;});
+
+    binding.transition()
+     .attr("transform", function(d, i) {
+        console.log(i*50);
+        return "translate(" + i * 50 + ",0)";
+       
+     });
+
+     console.log(binding);
+}
+
+
+function test() {
+    // var data = d3.range(8).map(()=>~~(Math.random()*130));
+    var data = input;
+
+    var svg = d3.select("svg")
+
+    var groups = svg.selectAll(".groups")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("class", "gbar");
+        
+    groups.append("rect")
+        .attr("x", (d,i)=> i * 50)
+        .attr("width", 40)
+        .attr("y", 20)
+        .attr("height", 40)
+        .attr("fill", "white")
+        .attr("line", "black");
+        
+    groups.append("text")
+        .attr("x", (d,i)=> i * 50 + 16)
+        .attr("y", 43)
+        .text(d=>d)
+}
+
+function create_hashmap_placeholder() {
+    $(".solution").append("<div>A</div>");
+}
+
+$(function(){
+    // updateChart();
+    test();
+    create_hashmap_placeholder();
+});
